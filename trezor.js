3927a88be0728025c084e3787050781be1a34e68
Jan Pochyla <jpochyla@gmail.com>
2014-02-27 17:56:51 +0100

    Generate just 32 bytes of entropy

--- trezor.js
+++ trezor.js
@@ -568,7 +568,7 @@ var TrezorApi = function(Promise) {
 
         if (res.type === 'EntropyRequest')
             return this._commonCall('EntropyAck', {
-                entropy: Hex.encode(this._generateEntropy(32))
+                entropy: Hex.encode(this._generateEntropy(256))
             });
 
         if (res.type === 'PinMatrixRequest')
