45d3d6bdbba79d6c2bab5405db5f5a2d55458d2f
Karel Bilek <kb@karelbilek.com>
2017-01-06 00:49:52 +0100

    Forgot to include webusb

--- src/index-browser.js
+++ src/index-browser.js
@@ -7,7 +7,7 @@ import 'unorm';
 import link from 'trezor-link';
 import DeviceList from './device-list';
 
-const {Bridge, Extension, Fallback} = link;
+const {Bridge, Extension, Lowlevel, WebUsb, Fallback} = link;
 
 export {default as Session} from './session';
 export {default as UnacquiredDevice} from './unacquired-device';
@@ -15,7 +15,7 @@ export {default as Device} from './device';
 export {default as DescriptorStream} from './descriptor-stream';
 export {default as DeviceList} from './device-list';
 
-DeviceList._setTransport(() => new Fallback([new Extension(), new Bridge()]));
+DeviceList._setTransport(() => new Fallback([new Extension(), new Bridge(), new Lowlevel(new WebUsb())]));
 
 import {setFetch as installersSetFetch} from './installers';
 DeviceList._setFetch(window.fetch);
